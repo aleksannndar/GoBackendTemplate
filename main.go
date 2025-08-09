f0763167845094e99bed2bc722f66c1602f71866
Aleksandar Milovanovic <aleksandar1034@yahoo.com>
2025-08-09 22:26:14 +0200

    Setup auth routes, rename migration package to migrate, add running migrations on server start and loading of .env

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"GoBackendTemplate/database/migrate"
 	"GoBackendTemplate/dependency"
 	"GoBackendTemplate/routes"
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env: %v", err)
+	}
+
+	err = migrate.RunMigrations()
+	if err != nil {
+		log.Fatalf("Failed to run migration: %v", err)
+	}
+
 	r := gin.Default()
-
 	container := dependency.BuildContainer()
-
 	routes.SetupRoutes(r, container)
 
 	if err := r.Run(":8088"); err != nil {
--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,13 @@ package routes
 
 import (
 	"GoBackendTemplate/dependency"
+	"GoBackendTemplate/domains/auth"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine, container *dependency.Container) {
-
+	authGroup := r.Group("auth")
+	authGroup.POST("/login", auth.LoginHandler(container.AuthService))
+	authGroup.POST("/register", auth.RegisterHandler(container.AuthService))
 }
